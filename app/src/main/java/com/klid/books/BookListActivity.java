59689edf6312b6e36a3950ff1f081a0b8f3dcea5
Ivan Kaptue <ivanokaptue@gmail.com>
2019-07-30 09:21:52 +0100

    feat: add searchView in action bar

--- app/src/main/java/com/klid/books/BookListActivity.java
+++ app/src/main/java/com/klid/books/BookListActivity.java
@@ -1,19 +1,22 @@
 package com.klid.books;
 
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.net.URL;
 import java.util.ArrayList;
 
-public class BookListActivity extends AppCompatActivity {
+public class BookListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private ProgressBar mLoadingProgress;
     private RecyclerView rvBooks;
@@ -31,8 +34,34 @@ public class BookListActivity extends AppCompatActivity {
             URL url = ApiUtil.buildUrl("java");
             new BooksQueryTask().execute(url);
         } catch (Exception e) {
-            Log.d("error" , e.getMessage());
+            Log.d("error", e.getMessage());
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        try {
+            URL bookUrl = ApiUtil.buildUrl(query);
+            new BooksQueryTask().execute(bookUrl);
+        }catch (Exception e) {
+            Log.e("error", e.getMessage());
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 
     public class BooksQueryTask extends AsyncTask<URL, Void, String> {
